8683f168b13b7c96b94cbc07f7308c5ddf62e4d9
Steffen <steffen@flaemig.de>
2023-10-02 09:19:02 +0200

    Test

--- totalEnergy.py
+++ totalEnergy.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
-fd = pd.read_csv("data\\bristell_001.csv")
-print(fd)
+fd = pd.read_csv("data\\bristell_001.csv", delimiter=";")
+print(fd.head())
+print(fd.dtypes)
+fd["iasMps"] = fd["ias"] * 1.852 / 3.6
+fd["tasMps"] = fd["tas"] * 1.852 / 3.6
+fd["gsMps"] = fd["gs"] * 1.852 / 3.6
+fd["vvMps"] = fd["vv"] * 0.00508
+print(fd.dtypes)
+print(fd[["vv", "vvMps"]])
